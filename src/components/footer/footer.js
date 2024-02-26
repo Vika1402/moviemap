3463e7db299ec9178218be5104eb5085a52c9f97
vikas <vikas.kumar123@ssipmt.com>
2024-02-27 03:24:57 +0530

    folder cretes like pages and  header footer inside

---
+++ src/components/footer/footer.js
@@ -0,0 +1,7 @@
+import "./footer.scss";
+// https://www.youtube.com/watch?v=VLgVw2NEqCM&ab_channel=JSDevHindi
+const footer = () => {
+  return <div>footer</div>;
+};
+
+export default footer;
---
+++ src/pages/serachResult/serachResult.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const serachResult = () => {
+  return (
+    <div>serachResult</div>
+  )
+}
+
+export default serachResult
