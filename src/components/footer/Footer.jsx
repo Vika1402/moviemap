dd59551315d656ac3fa79a04cf8a329edd2a7880
vikas <vikas.kumar123@ssipmt.com>
2024-03-02 01:26:51 +0530

    footer error solve

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,3 @@
-import "./Footer.scss";
 // https://www.youtube.com/watch?v=VLgVw2NEqCM&ab_channel=JSDevHindi
 import {
   FaFacebookF,
@@ -6,7 +5,7 @@ import {
   FaTwitter,
   FaLinkedin,
 } from "react-icons/fa";
-
+import "./Footer.scss";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 const Footer = () => {
@@ -21,7 +20,18 @@ const Footer = () => {
           <li className="menuItem">FAQ</li>
         </ul>
         <div className="infoText">
-        This website is currently under development and may contain incomplete or inaccurate information. The content provided on this website is for demonstration and testing purposes only. While efforts are made to ensure the accuracy and reliability of the information presented, we make no warranties or representations regarding the completeness, accuracy, suitability, or reliability of the websites content.Any action you take upon the information presented on this website is strictly at your own risk. We shall not be liable for any losses or damages arising from the use of this website or its contentPlease note that certain features, functionalities, or content may be subject to change without prior notice as development progresses.Thank you for your understanding and cooperation during the development phase.
+          This website is currently under development and may contain incomplete
+          or inaccurate information. The content provided on this website is for
+          demonstration and testing purposes only. While efforts are made to
+          ensure the accuracy and reliability of the information presented, we
+          make no warranties or representations regarding the completeness,
+          accuracy, suitability, or reliability of the websites content.Any
+          action you take upon the information presented on this website is
+          strictly at your own risk. We shall not be liable for any losses or
+          damages arising from the use of this website or its contentPlease note
+          that certain features, functionalities, or content may be subject to
+          change without prior notice as development progresses.Thank you for
+          your understanding and cooperation during the development phase.
         </div>
         <div className="socialIcons">
           <span className="icon">
